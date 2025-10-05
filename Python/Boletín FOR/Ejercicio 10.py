cc625275b41f4a56a2ff4e0c5936121a090f0636
Adri726-av <aleovel726@g.educaand.es>
2025-10-05 22:05:48 +0200

    Ejercicio 9 y 10

---
+++ Python/Boletín FOR/Ejercicio 10.py
@@ -0,0 +1,4 @@
+for i in range(5,51):
+    if i% 5== 0:
+        print(i)
+print("Fin")
---
+++ Python/Boletín FOR/Ejercicio 9.py
@@ -0,0 +1,4 @@
+for i in range(1,11):
+    cuadrado = i * i
+    print("El cuadrado de ", i , "es ", cuadrado)
+print("Fin")
