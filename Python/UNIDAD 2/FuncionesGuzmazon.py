d8578b632c3add9bcc0614edbfec6d779f479bd8
Adri726-av <aleovel726@g.educaand.es>
2025-11-13 14:28:01 +0100

    Subo

--- Python/UNIDAD 2/FuncionesGuzmazon.py
+++ Python/UNIDAD 2/FuncionesGuzmazon.py
@@ -1,13 +1,63 @@
 # cargaCesta
-def cargaCesta(dineroMax):
-    productos = []
-    precios = []
-    costeTotal = 0
+def cargaCesta(gastar):
+    lista_productos = []
+    lista_precios = []
+    lista_productos_superior = []
+    suma = 0.0
+    while suma < gastar:
+        producto = input("Introduce el producto: ")
+        lista_productos.append(producto)
+        precio = float(input("Introduce el precio del producto: "))
+        lista_precios.append(precio)
+        suma += precio
+        if suma > gastar:
+            lista_precios.remove(precio)
+            suma_total = suma - precio
+        if suma > gastar:
+            lista_productos.remove(producto)
 
-    return productos, precios, costeTotal
+    print(f"Importe máximo a gastar: {gastar}€")
+    print(f"Productos: {lista_productos}")
+    print(f"Precios: {lista_precios}")
+    print(f"Coste total de la cesta: {suma_total}")
+
+    return lista_productos, lista_precios, lista_productos_superior, gastar, suma_total
+
+def pinteMenu():
+    print("Pulse la opcion S para calcular el dinero sobrante")
+    print("Pulsa la opcion R para eliminar un producto y su precio de la lista")
+    print("Pulsa la opción C para devolver la lista de productos cuyo precio es mas alto que un importe")
+
+# leaOpciones
+def leaOpciones():
+    opcion = input("Introduce S / R / C:").upper()
+
+# calculeSobrante
+def calculeSobrante():
+    sobrante = gastar - suma_total
+    print(f"El dinero que te sobra es: {sobrante}")
+
+# eliminaProducto
+def eliminaProducto():
+    nombre = input("Introduce el nombre de un producto: ")
+    print(lista_productos)
+    print(lista_precios)
+    pregunta = input("Voy a eliminar el producto con ese nombre, ¿estas seguro? (introduce S o N): ").upper()
+    if pregunta == "S":
+        pos = lista_productos.index(nombre)
+        lista_productos.pop(pos)
+        lista_precios.pop(pos)
+    print(lista_productos)
+    print(lista_precios)
+
+# calculaListaPrecio
+
+# masAlto
 
 presupuesto = input("Dime cuanto tienes para gastarte como maximo")
 resultados = cargaCesta(presupuesto)
-productos = resultados[0]
-precios = resultados[1]
-costeTotal = resultados[2]
+lista_productos = resultados[0]
+lista_precios = resultados[1]
+lista_productos_superior = resultados[2]
+gastar = resultados[3]
+suma_total = resultados[4]
