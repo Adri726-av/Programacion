fc1cb61f590e36963a5d707661a01ba93294f4c5
Adri726-av <aleovel726@g.educaand.es>
2025-11-28 08:51:24 +0100

    subo

--- Python/UNIDAD 1/Boletín Tema 0/Ejercicio 3.py
+++ Python/UNIDAD 1/Boletín Tema 0/Ejercicio 3.py
@@ -2,3 +2,5 @@ numero1 = int(input ("Introduce un número para multiplicar:"))
 numero2 = int(input ("Introduce otro número para multiplcar:"))
 numero3 = numero1 * numero2
 print (numero3)
+
+print ("asco de programa")
